5404a033853fd8311d55aa9cb5f711da24206e51
Carlos André <moxiiru@gmail.com>
2020-12-27 17:54:56 -0300

    Ajustes e preparação para as próximas telas.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+import styles from './styles'
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containerRouts}>
       <StatusBar style="auto" />
     </View>
   );
---
+++ styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    containerRouts: {
+        flex: 1,
+        backgroundColor: '#F4F6F9',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
+
+export default styles;
