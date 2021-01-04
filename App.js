ef7209f82ddcbade5f92d82b4d29737388097732
Carlos André <moxiiru@gmail.com>
2021-01-04 18:42:33 -0300

    Correção da App.js

--- App.js
+++ App.js
@@ -6,15 +6,9 @@ import Routes from "./src/routes";
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    <View style={styles.containerRouts}>
-      <StatusBar style="light" />
-      <LoginScreenEmail />
-=======
     <View style = {{ flex : 1 }}>
       <StatusBar style="light" />
       <Routes/>
->>>>>>> login-screen
     </View>
   );
 }
