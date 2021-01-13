b837671c145ee8a9e82b88de2b0420bbf3fc0bff
Carlos André <moxiiru@gmail.com>
2021-01-13 20:51:31 -0300

    Componente de Input quase concluido.

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,87 +1,239 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { 
-    StyleSheet, 
-    View, 
-    TextInput, 
-    Text,
-    Animated
-} from 'react-native'
+import React, { Component, useState } from 'react';
+import PropTypes from 'prop-types';
 
-import {
-    white,
-    blue,
-    orange,
-    chumbo,
-    noEvidence
-} from './Colors'
+import { 
+    StyleSheet,
+    View,
+    Text, 
+    Animated,
+    TouchableOpacity
+} from 'react-native';
+
+import * as Colors from './Colors';
+import { TextInput } from 'react-native-gesture-handler';
+
+class PropsDefault extends Component {
+    static propTypes = {
+        keyboardType : PropTypes.string,
+        label : PropTypes.string.isRequired ,
+        placeholder : PropTypes.string,
+        notVisible : PropTypes.string,
+        visible : PropTypes.string,
+
+        mask : PropTypes.string ,
+        onLongPress : PropTypes.func,
+        marginHorizontal : PropTypes.number,
+        marginVertical : PropTypes.number,
+        
+        color : PropTypes.string,
+
+        leftIcon : PropTypes.elementType,
+        rightIcon : PropTypes.elementType,
+
+        labelSiza : PropTypes.number, // to control size of title when field is active
+        floatLabelSize : PropTypes.number, // to control size of title when field is inactive
+        labelFocusColor : PropTypes.string, // to control color of title when field is active
+        labelDefaultColor : PropTypes.string, // to control color of title when field is active
+        
+        errorText : PropTypes.string,
+        errorTextColor : PropTypes.string,
+    }
+}
 
 function createIconReact (icon, color) {
     if(icon != null){
-        return React.createElement(icon,{color : color})
+        return React.createElement(icon, {color : color, marginHorizontal : 8})
     }
     return
 }
 
-class InputOutlined extends Component {
-    static propTypes = {
-        marginHorizontal : PropTypes.number,
-        marginVertical : PropTypes.number,
-        color: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        placeholder: PropTypes.string,
-        onPress : PropTypes.func,
-        leftIcon : PropTypes.element,
-        rightIcon : PropTypes.element,
+class Input extends PropsDefault {
+
+    constructor(props) {
+        super(props);
+        const { value } = this.props;
+        this.position = new Animated.Value(value ? 1 : 0);
+        this.state = {
+            isFieldActive: false,
+            isVisible : false,
+        }
+    }
+    
+    _handleFocus = () => {
+        if (!this.state.isFieldActive) {
+            this.setState({ isFieldActive: true, isVisible: true });
+            Animated.timing(this.position, {
+                toValue: 1,
+                duration: 150,
+                useNativeDriver : false,
+            }).start();
+        }
+        console.log(this.state.isFieldActive + "  :  " + this.state.isVisible)
+    }
+    
+    _handleBlur = () => {
+        if (this.state.isFieldActive && !this.props.value) {
+            this.setState({ isFieldActive: false, isVisible: false });
+            Animated.timing(this.position, {
+                toValue: 0,
+                duration: 150,
+                useNativeDriver : false,
+            }).start();
+        }
+        console.log(this.state.isFieldActive + "  :  " + this.state.isVisible)
+    }
+    
+    _returnAnimatedTitleStyles = () => {
+        const { isFieldActive } = this.state;
+        const {
+            labelSiza = 12,
+            floatLabelSize = 15,
+            labelDefaultColor = Colors.blue, 
+            labelFocusColor = Colors.noEvidence,
+        } = this.props;
+
+        return {
+            top : this.position.interpolate({
+                inputRange: [0, 1],
+                outputRange: [20, -8],
+            }),
+            left : this.position.interpolate({
+                inputRange: [ 0, 1 ],
+                outputRange: [ 100, 30 ],
+            }),
+            fontSize : isFieldActive ? labelSiza : floatLabelSize,
+            color : isFieldActive ? labelDefaultColor : labelFocusColor,
+            paddingHorizontal : isFieldActive ? 4 : 10,
+        }
     }
 
-    render = () => {
-        const { 
+    _returnAnimatedPlaceholderStyles = () => {
+        const { isVisible } = this.state;
+        const {
+            notVisible = 'transparent',
+            visible = Colors.noEvidence,
+        } = this.props;
+
+        console.log("placeholder is visible : " + isVisible + "  : " + this.state.isVisible)
+
+        return isVisible ? visible : notVisible
+    }
+
+    _returnAnimatedBorderStyles = () => {
+        const { isFieldActive } = this.state;
+        const {
+            labelDefaultColor = Colors.blue, 
+            labelFocusColor = Colors.noEvidence,
+        } = this.props;
+    
+        return {
+            borderColor: isFieldActive ? labelDefaultColor : labelFocusColor,
+        }
+    }
+
+    _returnAnimatedIconStyles = () => {
+        const { isFieldActive } = this.state;
+        const {
+            labelDefaultColor = Colors.blue, 
+            labelFocusColor = Colors.noEvidence,
+        } = this.props;
+    
+        return isFieldActive ? labelDefaultColor : labelFocusColor
+    }
+
+    render() {
+        const {
+            keyboardType = 'default',
+            label,
+            placeholder,
+            mask,
+            onLongPress,
             marginHorizontal,
             marginVertical,
             color,
-            label,
-            placeholder,
-            onPress,
-            leftIcon, 
-            rightIcon 
+            leftIcon,
+            rightIcon,
+            errorText
         } = this.props
 
-        return(
-            <View style={[styles.container]}>
-                {/* <Text style={[styles.label, {color : color}]}>{label}</Text> */}
-                {createIconReact(leftIcon, color)}
-                <TextInput label={label} style={[styles.containerText, {color : color}]} placeholder={placeholder}/>
-                {createIconReact(rightIcon, color)}
+        return (
+            <View 
+                style={[ 
+                    styles.containerInput, 
+                    styles.borderSolidInInput, 
+                    styles.borderRadiusFull,
+                    this._returnAnimatedBorderStyles(),
+                    {
+                        marginHorizontal : marginHorizontal,
+                        marginVertical : marginVertical,
+                    }
+                ]}
+            >
+                <Animated.Text style={[ 
+                    styles.text,
+                    this._returnAnimatedTitleStyles(),
+                    ]}
+                >
+                    { label }
+                </Animated.Text>
+
+                { createIconReact(leftIcon, this._returnAnimatedIconStyles()) }
+
+                <TextInput 
+                    style={[ 
+                        styles.textInput,
+                        this._returnAnimatedPlaceholderStyles()
+                    ]}
+                    placeholder={placeholder}
+                    placeholderTextColor={this._returnAnimatedPlaceholderStyles()}
+                    onFocus = {this._handleFocus}
+                    onBlur = {this._handleBlur}
+                    keyboardType = { keyboardType }
+                />
+
+                { createIconReact(rightIcon, this._returnAnimatedIconStyles()) }
             </View>
         )
-        
     }
 }
 
 const styles = StyleSheet.create({
-    container : {
+    containerInput : {
         flex : 1,
         flexDirection : 'row',
-        padding : 8,
-        width: '100%',
-        height : '100%',
+        width : '100%',
         maxHeight : 60,
-        borderColor : blue,
+        padding : 8,
+        justifyContent : 'space-evenly',
+        alignItems : 'center',
+    },
+
+    borderSolidInInput : {
         borderWidth : 1,
     },
 
-    containerText : {
-        marginHorizontal : 8,
+    borderRadiusFull : {
+        borderRadius : 50,
+    },
+    
+    borderRadiusHalf : {
+        borderRadius : 15,
     },
 
-    label : {
-        flex : 1,
-        position: 'absolute',
-        top : '40%',
-        left : 8,
-        fontSize : 18,
+    text : {
+        position : 'absolute',
+        textAlign : 'center',
+        backgroundColor : Colors.white,
     },
+
+    textInput : {
+        flex : 3,
+        width : '100%',
+        height : '100%',
+        textAlign : 'center',
+    }
 })
 
-export { InputOutlined };
+export { 
+    Input
+};
