63e36617d930b25fd193827e03bb7b8d177a49d5
Carlos <moxiiru@gmail.com>
2021-03-12 11:52:23 -0300

    Start das TabsScreens

--- src/routes.js
+++ src/routes.js
@@ -10,14 +10,21 @@ import FristStep from './screens/RegisterScreen/FristStep';
 import LastStep from './screens/RegisterScreen/LastStep';
 import LoginScreenEmail from './screens/LoginScreenEmail';
 
+import DashboardScreen from './screens/DashboardScreen';
+import SalesScreen from './screens/SalesScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
 const Tab = createBottomTabNavigator();
 
 function ScreenTab() {
     return(
-        <Tab.Navigator 
+        <Tab.Navigator
             screenOptions={{ headerShown : false }}>
-            <Tab.Screen name="LoginScreen" component={ LoginScreen } />
-            <Tab.Screen name="LoginScreenEmail" component={ LoginScreenEmail } />
+            <Tab.Screen name="Dashboard" component={ DashboardScreen } />
+            <Tab.Screen name="Vendas" component={ SalesScreen } />
+            <Tab.Screen name="Produtos" component={ ProductsScreen } />
+            <Tab.Screen name="Configurações" component={ SettingsScreen } />
         </Tab.Navigator>
     )
 }
---
+++ src/screens/DashboardScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './styles'
+
+export default function DashboardScreen () {
+    return (
+        <View style={{ justifyContent : 'center', alignItems : 'center', flex : 1 }}> 
+            <Text>Dashboard</Text>
+        </View>
+    )
+}
