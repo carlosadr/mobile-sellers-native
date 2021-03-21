19141879d6fc56f54f6e8b6030798bdbc2a45218
carlosa <carlosa.reboucas@gmail.com>
2021-03-21 17:40:57 -0300

    component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Bell } from 'react-feather';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { Bell } from 'react-native-feather'
+
 import * as Colors from '../utils/Colors';
+import Button from '../Button';
 
 interface HeaderProps {
     data : Object,
 }
 
 const Header: React.FC<HeaderProps> = ({ data }) => {
+
+    function createIconReact(icon, color) {
+        return icon != null ? React.createElement(icon, { color: color }) : null
+    }
+
     return (
         <>
             <View style={ styles.containerLabel }>
-                <Image style={ styles.imageLogo } source={ require('../../../assets/logo.png') } />
-                <TouchableOpacity >
-                    <Bell size={ 16 }/>
-                </TouchableOpacity>
+                <Image style={ styles.imageLogo } source={ require('../../../assets/logo-marca.png') } />
+                <Button type="text" label="" color="orange" leftIcon={ Bell } />
+            </View>
+            <View style={ styles.containerLabel } >
+                <Image source={require('../../../assets/adaptive-icon.png')} />
+                <View >
+                    <Text>
+                        Titulo do Plano
+                    </Text>
+                    <Text>
+                        Preço do Plano
+                    </Text>
+                </View>
             </View>
         </>
     )
@@ -33,6 +48,8 @@ const styles = StyleSheet.create({
 
     imageLogo : {
         flex : 1,
+        width : 50,
+        height : 50,
 
         borderWidth : 1,
     }
